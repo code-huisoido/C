ac9f65176aad2204d246b329775db45e03dca22b
huisoido <584331125@qq.com>
2016-08-06 19:02:30 +0800

    20160806

--- 20160806/getCelsiusByFahr.c
+++ 20160806/getCelsiusByFahr.c
@@ -3,7 +3,7 @@
 /*当fahr=0, 20, ..., 300时，分别打印华氏温度与摄氏温度对照表*/
 main()
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;		/*	温度表的下限	*/
@@ -12,8 +12,8 @@ main()
 
 	fahr = lower;
 	while(fahr <= upper){
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
