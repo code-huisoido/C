f8db56abb9745d4653312adfcb7b182a5576cb1e
huisoido <584331125@qq.com>
2017-09-16 12:09:36 +0800

    docs: 1-10

---
+++ 1/1-10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+    int c;
+    c = 0;
+
+    while ((c = getchar()) != EOF) 
+    {   
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        }
+        if (c == '\b') {
+            putchar('\\');
+            putchar('b');   
+        }
+
+        if (c != '\t') {
+            if (c != '\b') {
+                putchar(c);
+            }
+        }
+    }
+}
